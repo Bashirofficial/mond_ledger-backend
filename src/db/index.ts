704728f3bb97d6b08a480e2444b834398fc1caea
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-04 16:02:51 +0530

    fix: import Prisma Client is being fixed

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/client/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
