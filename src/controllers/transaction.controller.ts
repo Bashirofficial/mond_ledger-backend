37872fc5ae26eac14c914e703455dc082d493bb8
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-07 15:54:33 +0530

    new: category controller is being added

---
+++ src/controllers/transaction.controller.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from "express";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import txService from "../services/transaction.service.js";
+
+// User helper function to extract user info from request
+const getUser = (req: Request) => ({ id: req.user!.id, role: req.user!.role });
+
+// -------- Controller Methods --------
+
+const createTransaction = AsyncHandler(async (req: Request, res: Response) => {
+  const user = getUser(req);
+
+  const transaction = await txService.createTransaction(req.body, user);
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, { transaction }, "Transaction created successfully"),
+    );
+});
+
+const getTransactions = AsyncHandler(async (req: Request, res: Response) => {
+  const user = getUser(req);
+
+  const transactions = await txService.getTransactions(req.query, user);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, transactions, "Transactions retrieved successfully"),
+    );
+});
+
+const updateTransaction = AsyncHandler(async (req: Request, res: Response) => {
+  const user = getUser(req);
+  const transactionId = req.params.id as string;
+
+  const updated = await txService.updateTransaction(
+    transactionId,
+    req.body,
+    user,
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { transaction: updated },
+        "Transaction updated successfully",
+      ),
+    );
+});
+
+const deleteTransaction = AsyncHandler(async (req: Request, res: Response) => {
+  const user = getUser(req);
+  const transactionId = req.params.id as string;
+
+  await txService.deleteTransaction(transactionId, user);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Transaction deleted successfully"));
+});
+
+const getDashboardData = AsyncHandler(async (req: Request, res: Response) => {
+  const user = getUser(req);
+
+  const summary = await txService.getDashboardData(user.id);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, summary, "Summary retrieved successfully"));
+});
+
+export {
+  createTransaction,
+  getTransactions,
+  updateTransaction,
+  deleteTransaction,
+  getDashboardData,
+};
