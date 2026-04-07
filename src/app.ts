54e545cc462c7e91e6c75774d035b62fa0c860aa
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-08 00:10:39 +0530

    add: auditLog router is being added in app file

--- src/app.ts
+++ src/app.ts
@@ -42,10 +42,12 @@ app.get("/health", (req, res) => {
 import userRoutes from "./routes/user.route.js";
 import categoryRoutes from "./routes/category.route.js";
 import transactionRoutes from "./routes/transaction.route.js";
+import auditLogRoutes from "./routes/auditLog.rotue.js";
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/transactions", transactionRoutes);
+app.use("/api/v1/audit-logs", auditLogRoutes);
 
 // ================= Error Handling =================
 app.use(notFoundHandler); // 404 handler - must be after all routes
