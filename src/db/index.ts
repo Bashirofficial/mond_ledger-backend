8ab38fe8f92cdf69e34172afe78c79f1ff0bc985
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-07 15:55:38 +0530

    fix: imported prisma client with .js extension to resolve errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "../generated/client/client";
+import { PrismaClient } from "../generated/client/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export const connectDB = async () => {
